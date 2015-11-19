13c2f87dafb3efc031b87cc64ba1fc8abaf38d2c
MA Laforge <ma.laforge@example.com>
2015-11-19 15:49:16 -0500

    Demonstrate xcross, ycross, xcross1 & ycross1

--- sample/demo9.jl
+++ sample/demo9.jl
@@ -22,6 +22,11 @@ ncycles = 5
 ===============================================================================#
 t = DataF1(0:(2pi/osr):(ncycles*(2pi)))
 y = cos(t)
+dydt = deriv(y)
+∫ydt = iinteg(y)
+x2x=xcross1(y, ∫ydt, n=2)
+x2y=ycross1(y, ∫ydt, n=2)
+println("crossing#2: ($x2x, $x2y)")
 
 #Generate parameter sweeps:
 sweeplist = PSweep[
@@ -44,8 +49,9 @@ end
 plot=EasyPlot.new(title="Integration/Differentiation/yvsx")
 s = add(plot, vvst, title="Integration/Differentiation")
 	add(s, y, id="sig")
-	add(s, deriv(y), id="dy/dt")
-	add(s, iinteg(y), id="integ(y)dt")
+	add(s, dydt, id="dy/dt")
+	add(s, ∫ydt, id="integ(y)dt")
+	add(s, ycross(y, dydt), line(style=:none), glyph(shape=:o, color=:red), id="crossings")
 s = add(plot, vvst, title="Lissajous Curves")
 	add(s, yvsx(ydel,yref))
 
