6dc04080465ad45e25ec3494cf74e91e7306d56d
MA Laforge <ma.laforge@example.com>
2015-11-30 19:58:53 -0500

    Add ability to save plots.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@
 
 #No real test code yet... just run demos:
 
+using FileIO2
 using MDDatasets
 import MDDatasets: DS
 using EasyPlot
@@ -24,20 +25,25 @@ if in(:Grace, plotlist)
 		render(gplot, plot, ncols=ncols); display(gplot)
 		return gplot
 	end
+
+	function DisplayDemoPlot(ds::DS{:Grace}, plot::EasyPlot.Plot, ncols::Int, outfile)
+		gplot = DisplayDemoPlot(ds, plot, ncols)
+		if outfile != nothing; save(gplot, outfile); end
+	end
 end
 
 #Only import EasyPlotMPL (long load time) if desired: 
 if in(:MPL, plotlist)
 	import EasyPlotMPL
 
-	function DisplayDemoPlot(::DS{:MPL}, plot::EasyPlot.Plot, ncols::Int)
+	function DisplayDemoPlot(::DS{:MPL}, plot::EasyPlot.Plot, ncols::Int, args...)
 		display(:MPL, plot, ncols=ncols);
 	end
 end
 
-function DisplayDemoPlot(plotlist::Set, plot::EasyPlot.Plot, ncols::Int)
+function DisplayDemoPlot(plotlist::Set, plot::EasyPlot.Plot, ncols::Int, args...)
 	for plottype in plotlist
-		DisplayDemoPlot(DS(plottype), plot, ncols)
+		DisplayDemoPlot(DS(plottype), plot, ncols, args...)
 	end
 end
 
@@ -46,9 +52,11 @@ end
 ===============================================================================#
 for i in 1:12
 	file = "../sample/demo$i.jl"
+	outfile = File{PNGFmt}(joinpath("./", splitext(basename(file))[1] * ".png"))
 	println("\nExecuting $file...")
 	(plot, ncols) = evalfile(file)
 	DisplayDemoPlot(plotlist, plot, ncols)
+#	DisplayDemoPlot(plotlist, plot, ncols, outfile)
 end
 
 
