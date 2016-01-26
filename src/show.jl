c58695e904343902ca899ff31a087a4b79d49004
MA Laforge <ma.laforge@example.com>
2016-01-25 19:15:28 -0500

    Improve how network parameters are displayed.

--- src/show.jl
+++ src/show.jl
@@ -7,27 +7,40 @@
 
 Base.show{NP}(io::IO, t::Type{NPType{NP}}) =	print(io, "NPType{:$NP}")
 
+_printmatrix{T<:Number}(io::IO, m::NetworkParameterMatrix{T}) = println(io, m)
+function _printmatrix(io::IO, m::NetworkParameterMatrix)
+	nports = portcount(m)
+	for row = 1:nports, col = 1:nports
+		print(io, "  ($row,$col)=")
+		if isdefined(m, row, col)
+			println(io, m[row, col])
+		else
+			println(io, "UNDEFINED")
+		end
+	end
+end
+
 function Base.show(io::IO, np::SParameters)
 	println(io, "SParameters{$(portcount(np))-port, z0=$(np.z0)}[")
-	println(io, np.m)
+	_printmatrix(io, np.m)
 	println(io, "]")
 end
 
 function Base.show(io::IO, np::ZParameters)
 	println(io, "ZParameters{$(portcount(np))-port}[")
-	println(io, np.m)
+	_printmatrix(io, np.m)
 	println(io, "]")
 end
 
 function Base.show(io::IO, np::YParameters)
 	println(io, "YParameters{$(portcount(np))-port}[")
-	println(io, np.m)
+	_printmatrix(io, np.m)
 	println(io, "]")
 end
 
 function Base.show(io::IO, np::ABCDParameters)
 	println(io, "ABCDParameters[")
-	println(io, np.m)
+	_printmatrix(io, np.m)
 	println(io, "]")
 end
 
