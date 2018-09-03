b7c6df1d2c83b193cf4aa53f733ef1fcabd6ea00
MA Laforge <ma.laforge.49@gmail.com>
2018-09-03 09:10:44 -0700

    Disambiguate which "*" or "/" to use.

--- src/base.jl
+++ src/base.jl
@@ -38,6 +38,15 @@ const TCapacitance = DataTag{:C}
 (::Type{DataTag{ID}})(v::T) where {ID, T} = DataTag{ID, T}(v)
 
 
+#==Helper functions
+===============================================================================#
+#multMD: disambiguate multiplication to use
+multMD(a, b) = a*b #DataMD values don't require .*
+multMD(a::AbstractArray, b::AbstractArray) = a .* b
+divMD(a, b) = a/b #DataMD values don't require .*
+divMD(a::AbstractArray, b::AbstractArray) = a ./ b
+
+
 #==Operations
 ===============================================================================#
 
@@ -74,18 +83,18 @@ admittance(v) = TAdmittance(v)
 capacitance(v) = TCapacitance(v)
 inductance(v) = TInductance(v)
 
-impedance(y::TAdmittance) = impedance(1 ./ y.v)
-admittance(z::TImpedance) = admittance(1 ./ z.v)
+impedance(y::TAdmittance) = impedance(divMD(1, y.v))
+admittance(z::TImpedance) = admittance(divMD(1, z.v))
 
 #Capacitor values:
-_Y(c::TCapacitance, f) = admittance((2*pi*c.v)im.*f)
+_Y(c::TCapacitance, f) = admittance(multMD((2*pi*c.v)im, f))
 _Y(c::TCapacitance, ::Nothing) = throw(ArgumentError("Missing kwarg :f"))
 admittance(c::TCapacitance; f = nothing) = _Y(c, f)
 impedance(c::TCapacitance, args...; kwargs...) = impedance(admittance(c, args...; kwargs...))
 #TODO: create admittance(:L, value, f=x)??
 
 #Inductor values:
-_Z(l::TInductance, f) = admittance((2*pi*c.v)im.*f)
+_Z(l::TInductance, f) = admittance(multMD((2*pi*c.v)im, f))
 _Z(l::TInductance, ::Nothing) = throw(ArgumentError("Missing kwarg :f"))
 impedance(l::TInductance; f = nothing) = _Z(l, f)
 admittance(l::TInductance, args...; kwargs...) = admittance(impedance(l, args...; kwargs...))
